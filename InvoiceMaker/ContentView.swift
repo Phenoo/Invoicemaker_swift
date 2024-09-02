0f5b47f9cec244308288c2897b9b25d2d49ee864
Phenoo <descometusah@gmail.com>
2024-09-03 00:12:51 +0100

    Initial Commit

---
+++ InvoiceMaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InvoiceMaker
+//
+//  Created by Eze Chidera Paschal on 03/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InvoiceMaker/InvoiceMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoiceMakerApp.swift
+//  InvoiceMaker
+//
+//  Created by Eze Chidera Paschal on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct InvoiceMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
